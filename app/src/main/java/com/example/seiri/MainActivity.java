b1f805426673c00bfe5ee8f5c9385cfa50f6b851
Gurvan B <gurvan.buanic123@gmail.com>
2023-01-22 18:05:41 +0100

    Add page for add food product and add function for add, delete, update, and count food product

--- app/src/main/java/com/example/seiri/MainActivity.java
+++ app/src/main/java/com/example/seiri/MainActivity.java
@@ -1,12 +1,51 @@
 package com.example.seiri;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import com.example.seiri.BD.FoodProduct;
+import com.example.seiri.BD.FoodProductViewModel;
+
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
-
+    private FoodProductViewModel foodProductViewModel;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        foodProductViewModel = new ViewModelProvider(this).get(FoodProductViewModel.class);
+        foodProductViewModel.getNbFoodProduct().observe(this, new Observer<Integer>() {
+            @Override
+            public void onChanged(Integer integer) {
+                TextView textView = findViewById(R.id.tvNbFoodProduct);
+                textView.setText(integer + "Aliments dans votre liste :");
+            }
+        });
     }
+
+    public void addFoodProduct(View view) {
+        Intent intent = new Intent(view.getContext(), AddFoodProduct.class);
+        startActivity(intent);
+    }
+
+    public void updateFoodProduct(View view) {
+        Intent intent = new Intent(view.getContext(), AddFoodProduct.class);
+        startActivity(intent);
+    }
+
+    public void deleteAllFoodProduct(View view) {
+        foodProductViewModel.deleteAllFoodProduct();
+    }
+
+    // à compléter
+//    public void deleteFoodProduct(View view) {
+//        foodProductViewModel.deleteFoodProduct(foodProduct);
+//    }
 }
---
+++ app/src/main/java/com/example/seiri/UpdateFoodProduct.java
@@ -0,0 +1,14 @@
+package com.example.seiri;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class UpdateFoodProduct extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_update_food_product);
+    }
+}
